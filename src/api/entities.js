d9ef676eceaa3e76701b9b36c035353a358bb30d
ohtheirany <ira.moves@proton.me>
2025-08-29 15:03:31 -0400

    Update entities.js

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,11 +1,15 @@
-import { base44 } from './base44Client';
+// import { base44 } from './base44Client';
 
+// Mock base44 object
+const base44 = {
+  entities: {
+    Item: { findMany: () => Promise.resolve([]) },
+    MiniReflection: { findMany: () => Promise.resolve([]) }
+  },
+  auth: { user: null }
+};
 
+// Rest of the file stays the same
 export const Item = base44.entities.Item;
-
 export const MiniReflection = base44.entities.MiniReflection;
-
-
-
-// auth sdk:
 export const User = base44.auth;
